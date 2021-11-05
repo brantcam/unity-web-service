99c220120766c29eb39e6a47c40cc697885324fe
brantcam <bmcampodonico@gmail.com>
2021-11-05 14:50:07 -0500

    removing queue image and using base

--- build.sh
+++ build.sh
@@ -6,10 +6,8 @@ IMAGE=brantcam/unity-web-service:$VERSION
 QUEUE_IMAGE=brantcam/rabbit-mq:v2
 
 docker build -t $IMAGE .
+docker scan $IMAGE --accept-license --json
 docker push $IMAGE
-
-docker build -f Dockerfile-queue -t $QUEUE_IMAGE .
-docker push $QUEUE_IMAGE
 
 sed "s/{VERSION}/$VERSION/g" k8s/k8s-in.yml > k8s/k8s.yml
 
